88cd4a2528269656d7ff17655c36689d6647448e
Philip Bollen <flip.philip@hotmail.com>
2022-11-30 21:28:32 +0100

    Make voting easy with simple pure functions

---
+++ strategic_voting/simple_voting.py
@@ -0,0 +1,61 @@
+from itertools import permutations
+from typing import Any
+from strategic_voting.types.voter import Voter
+
+from strategic_voting.util import cache, profiler
+
+
+@profiler.profile
+def tied(outcome: list[int]):
+    return outcome.count(max(outcome)) != 1
+
+
+@profiler.profile
+def alternative_voting_options(vote: tuple[int]):
+    all_options = permutations(vote)
+    filtered_options = set(all_options) - {vote}
+    for a in sorted(filtered_options):
+        yield a
+
+
+@profiler.profile
+def get_winner(outcome: list[int], options: list[Any]):
+    return max(zip(outcome, options), key=lambda z: z[0])
+
+
+@profiler.profile
+def voting_outcome(votes: list[tuple[int, ...]]):
+    return [sum(x) for x in zip(*votes)]
+
+
+@profiler.profile
+def strategic_voting_options(situation, calc):
+    all_votes = situation.votes(calc)
+    true_outcome = voting_outcome(all_votes)
+
+    voting_options = sorted(situation.voters[0].order)
+
+    options: dict[Voter, list[tuple[int, ...]]] = {}
+    for i, voter in enumerate(situation.voters):
+
+        options[voter] = []
+
+        if get_winner(true_outcome, voting_options)[1] == voter.order[0]:
+            # If first prefference is winner, we don't do anything
+            continue
+
+        voter.happiness = get_winner(true_outcome, voting_options)[1]
+        true_voter_happiness = voter.happiness
+
+        for option in alternative_voting_options(calc.calc(voter)):
+
+            temp = all_votes[i]
+            all_votes[i] = option
+            outcome = voting_outcome(all_votes)
+            all_votes[i] = temp
+
+            voter.happiness = get_winner(outcome, voting_options)[1]
+            if voter.happiness > true_voter_happiness:
+                options[voter].append(option)
+
+    return options
